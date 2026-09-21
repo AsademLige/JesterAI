5a40de833ba49ce09d5f4a00fdd2c5d6971acd89
Илья Яблоков <i.yablokov@rdckrista.ru>
2026-09-22 01:54:29 +0300

    - Фикс парсинга модели боя;

--- src/features/battles/data/models/battle_dto.py
+++ src/features/battles/data/models/battle_dto.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, ConfigDict, model_validator
+from typing import Any, List
 import json
 
 class Battle(BaseModel):
@@ -9,45 +10,27 @@ class Battle(BaseModel):
     user_id: int
     status: str = "none"
     data: dict = {}
-    log: dict = {}
+    log: List[Any] = []
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     @classmethod
-    def parse_json_fields(cls, value):
+    def parse_json_fields(cls, value: Any) -> Any:
         if not isinstance(value, dict):
-            for field_name in ['data', 'log']:
+            for field_name in ["data", "log"]:
                 raw_val = getattr(value, field_name, None)
                 if isinstance(raw_val, str):
                     try:
                         setattr(value, field_name, json.loads(raw_val))
                     except json.JSONDecodeError:
-                        setattr(value, field_name, {})
+                        default = [] if field_name == "log" else {}
+                        setattr(value, field_name, default)
             return value
 
-        for field_name in ['data', 'log']:
+        for field_name in ["data", "log"]:
             if isinstance(value.get(field_name), str):
                 try:
                     value[field_name] = json.loads(value[field_name])
                 except json.JSONDecodeError:
-                    value[field_name] = {}
-                    
+                    value[field_name] = [] if field_name == "log" else {}
+
         return value
-    
-    @model_validator(mode='before')
-    @classmethod
-    def parse_json_attributes(cls, log_dict):
-        if not isinstance(log_dict, dict):
-            raw_data = getattr(log_dict, 'log', None)
-            if isinstance(raw_data, str):
-                try:
-                    log_dict.log = json.loads(raw_data)
-                except json.JSONDecodeError:
-                    log_dict.log = {}
-        
-        elif isinstance(log_dict.get('log'), str):
-            try:
-                log_dict['log'] = json.loads(log_dict['log'])
-            except json.JSONDecodeError:
-                log_dict['log'] = {}
-                
-        return log_dict
